36da7ca10f82bd22cc5fe2af52f69a740f81b9de
Rajat <guptraj15@gmail.com>
2022-02-02 19:05:12 +0530

    rotate by 90 added

--- rotateBy90.java
+++ rotateBy90.java
@@ -3,6 +3,16 @@ import java.util.Scanner;
 public class rotateBy90 {
     public static Scanner scn = new Scanner(System.in);
 
+    public static void display(int[][] A) {
+        for (int i = 0; i < A.length; i++) {
+            for (int j = 0; j < A[0].length; j++) {
+                System.out.print(A[i][j] + " ");
+                
+            }
+            System.out.println();
+        }
+    }
+
     public static void takeInput(int[][] a) {
         for (int i = 0; i < a.length; i++) {
             for (int j = 0; j < a[0].length; j++) {
@@ -11,14 +21,52 @@ public class rotateBy90 {
         }
 
     }
-    
+
+    public static void reverse(int[][] a) {
+        for(int i=0;i<a.length; i++)
+        {
+            int x= 0;
+            int y=a[0].length-1;
+            while(x<y)
+            {
+                int temp = a[i][x];
+                a[i][x] = a[i][y];
+                a[i][y] = temp;
+                x++;
+                y--;
+
+            }
+        }
+        
+    }
+
+    public static void rbn(int[][] a) {
+
+        int n = a.length;
+        int m = a[0].length;
+
+        for (int i = 0; i < n; i++) {
+
+            for (int j = 0; j < i; j++) {
+                if (i > j) {
+                    int temp = a[i][j];
+                    a[i][j] = a[j][i];
+                    a[j][i] = temp;
+                }
+            }
+        }
+
+    }
+
     public static void main(String[] args) {
 
         int n1 = scn.nextInt();
-        int m1 = scn.nextInt();
-        int[][] A = new int[n1][m1];
+        int[][] A = new int[n1][n1];
         takeInput(A);
+        rbn(A);
+        reverse(A);
+        display(A);
 
     }
-    
+
 }
